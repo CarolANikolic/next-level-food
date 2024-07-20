28978caafa2502cd600922f411fe34563b31f4c7
Carol A. Nikolic <carolnikolic@gmail.com>
2024-07-20 23:47:23 +0200

    feat: create toggle menu for small screens.

--- components/header/main-header.js
+++ components/header/main-header.js
@@ -1,40 +1,47 @@
+'use client';
 import Link from "next/link";
 import Image from "next/image";
 import logoImg from "@/public/images/icons/logo.png";
 import styles from "./main-header.module.css";
 import MainHeaderBackground from "../main-header-background/main-header-background";
 import NavLink from "../nav-link/nav-link";
+import MenuIcon from "../menu-icon/menu-icon";
+import { useState } from "react";
 
 export default function MainHeader() {
+    const [menu, setMenu] = useState(false);
+
     return (
         <>
             <MainHeaderBackground/>
             <header className={styles.header}>
-                <div>
-                <Link href="/" className={styles.logoContainer}>
-                    <div>
-                        <Image
-                            className={styles.logo}
-                            src={logoImg}
-                            alt="A plate full of food."
-                            priority
-                        />
-                    </div>
-                    NextLevel Food
-                </Link>
-                </div>              
+                <div className={styles.logoDiv}>
+                    <Link href="/" className={styles.logoContainer}>
+                        <div>
+                            <Image
+                                className={styles.logo}
+                                src={logoImg}
+                                alt="A plate full of food."
+                                priority
+                            />
+                        </div>
+                        NextLevel Food
+                    </Link>
+                </div> 
 
-            <nav className={styles.nav}>
-                <ul>
-                <li>
-                    <NavLink href="/meals">Browse Meals</NavLink>
-                </li>
-                <li>
-                    <NavLink href="/community">Foodies Community</NavLink>
-                </li>
-                </ul>
-            </nav>
-    </header>
+                <MenuIcon toggleMenu={{ menu, setMenu}}/>
+
+                <nav className={`${styles.nav} ${menu && styles.showMenu}`}>
+                    <ul>
+                        <li>
+                            <NavLink href="/meals">Browse Meals</NavLink>
+                        </li>
+                        <li>
+                            <NavLink href="/community">Foodies Community</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
         </>
 );
 }
---
+++ components/menu-icon/menu-icon.js
@@ -0,0 +1,20 @@
+
+import styles from "./menu-icon.module.css"
+
+export default function MenuIcon(props) {
+    const {menu, setMenu} = props.toggleMenu;
+
+    return (
+        <>
+            <div 
+            className={`
+                ${styles.menuDropdownIcon} 
+                ${menu && styles.changeMenu}`}
+            onClick={() => setMenu(!menu)}>
+                <div className={styles.bar1}></div>
+                <div className={styles.bar2}></div>
+                <div className={styles.bar3}></div>
+            </div>       
+        </>
+    )
+}
